e040746e60c858fa59a09203c6588cc1f9312558
Md. Mehedi Hasan <md.mehedi@hungrynaki.com>
2023-01-03 17:23:58 +0600

    Initial Commit

---
+++ AVAdsPlayer/AVAdsPlayer.h
@@ -0,0 +1,18 @@
+//
+//  AVAdsPlayer.h
+//  AVAdsPlayer
+//
+//  Created by Mehedi Hasan on 3/1/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AVAdsPlayer.
+FOUNDATION_EXPORT double AVAdsPlayerVersionNumber;
+
+//! Project version string for AVAdsPlayer.
+FOUNDATION_EXPORT const unsigned char AVAdsPlayerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AVAdsPlayer/PublicHeader.h>
+
+
